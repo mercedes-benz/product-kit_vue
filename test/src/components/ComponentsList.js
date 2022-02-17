24431ac41d16a1f35fdb963328744368ab31fe6c
lukashar <lukas.harsch@mercedes-benz.com>
2022-02-17 14:30:29 +0100

    Test page added

---
+++ test/src/components/ComponentsList.js
@@ -0,0 +1,162 @@
+/**
+ * This components list was used and modified from user Joouis on github:
+ * https://github.com/Joouis/vuetify-playground/blob/master/src/components/UIComponentsList.js
+ * License: MIT
+ * @todo license text?
+ */
+export const COMPONENTS_MANIFEST = [
+    {
+        title: 'Alerts'
+    }, {
+        title: 'Avatars'
+    }, {
+        title: 'Badges'
+    }, {
+        title: 'Banners'
+    }, {
+        title: 'Bars',
+        items: [
+            {
+                title: 'App Bars'
+            }, {
+                title: 'Toolbars'
+            }, {
+                title: 'System Bars'
+            }]
+    }, {
+        title: 'Bottom navigation'
+    }, {
+        title: 'Bottom sheets'
+    }, {
+        title: 'Breadcrumbs'
+    }, {
+        title: 'Buttons'
+    }, {
+        title: 'Floating action buttons'
+    }, {
+        title: 'Calendars'
+    }, {
+        title: 'Cards'
+    }, {
+        title: 'Carousels'
+    }, {
+        title: 'Chips'
+    }, {
+        title: 'Dialogs'
+    }, {
+        title: 'Dividers'
+    }, {
+        title: 'Expansion panels'
+    }, {
+        title: 'Footer'
+    }, {
+        title: 'Grids'
+    }, {
+        title: 'Groups',
+        items: [{
+            title: 'Button groups'
+        }, {
+            title: 'Chip groups'
+        }, {
+            title: 'Item groups'
+        }, {
+            title: 'List item groups'
+        }, {
+            title: 'Slide groups'
+        }, {
+            title: 'Windows'
+        }]
+    }, {
+        title: 'Hover'
+    }, {
+        title: 'Icons'
+    }, {
+        title: 'Images'
+    }, {
+        title: 'Form inputs & controls',
+        items: [{
+            title: 'Autocompletes'
+        }, {
+            title: 'Combobox'
+        }, {
+            title: 'File inputs'
+        }, {
+            title: 'Forms'
+        }, {
+            title: 'Inputs'
+        }, {
+            title: 'Overflow buttons'
+        }, {
+            title: 'Selects'
+        }, {
+            title: 'Selection controls'
+        }, {
+            title: 'Sliders'
+        }, {
+            title: 'Textareas'
+        }, {
+            title: 'Text fields'
+        }]
+    }, {
+        title: 'Lazy'
+    }, {
+        title: 'Lists'
+    }, {
+        title: 'Menus'
+    }, {
+        title: 'Navigation drawers'
+    }, {
+        title: 'Overlays'
+    }, {
+        title: 'Paginations'
+    }, {
+        title: 'Parallax'
+    }, {
+        title: 'Pickers',
+        items: [{
+            title: 'Color pickers'
+        }, {
+            title: 'Date pickers'
+        }, {
+            title: 'Time pickers'
+        }]
+    }, {
+        title: 'Progress',
+        items: [{
+            title: 'Progress circular'
+        }, {
+            title: 'Progress linear'
+        }]
+    }, {
+        title: 'Ratings'
+    }, {
+        title: 'Sheets'
+    }, {
+        title: 'Skeleton Loaders'
+    }, {
+        title: 'Snackbars'
+    }, {
+        title: 'Sparklines'
+    }, {
+        title: 'Steppers'
+    }, {
+        title: 'Subheaders'
+    }, {
+        title: 'Tables',
+        items: [{
+            title: 'Data iterators'
+        }, {
+            title: 'Simple tables'
+        }, {
+            title: 'Data tables'
+        }]
+    }, {
+        title: 'Tabs'
+    }, {
+        title: 'Timelines'
+    }, {
+        title: 'Tooltips'
+    }, {
+        title: 'Treeview'
+    }
+];
---
+++ test/src/components/examples/ExampleWrapper.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="pl-3 pr-3 mb-6">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.v-subheader {
+  padding: 0;
+}
+</style>>
