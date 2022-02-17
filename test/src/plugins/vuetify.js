dacb11e36cde8d0fab1c6f43c38496e3d7d63c05
lukashar <lukas.harsch@mercedes-benz.com>
2022-02-17 22:42:31 +0100

    Initial setup and preparations for token support

--- test/src/plugins/vuetify.js
+++ test/src/plugins/vuetify.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import { preset } from './index.js'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    preset
 });
--- test/vue.config.js
+++ test/vue.config.js
@@ -1,5 +1,22 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  /*css: {
+    loaderOptions: {
+      scss: { // 9.0.0
+        additionalData: `@import "@/styles/overrides.scss";`,
+      },
+      sass: { // 9.0.0
+        additionalData: `@import "@/styles/overrides.scss"`,
+      },
+    },
+  },*/
+  /*css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "@/styles/_variables.scss"`
+      }
+    }
+  }*/
 }
