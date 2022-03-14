42ad00a531640909d8ea1fe37134a4dada5c47ab
lukashar <lukas.harsch@mercedes-benz.com>
2022-03-14 08:46:02 +0100

    Fixed bug from installation imports

--- test/vue.config.js
+++ test/vue.config.js
@@ -15,11 +15,15 @@ module.exports = {
     loaderOptions: {
       scss: { // 9.0.0
         additionalData: `
+          @use "@daimler/productkit-core/exports/web/styles/scss/variables" as tokens;
+          @use "@daimler/productkit-core/exports/web/styles/scss/variables-dark" as tokensDark;
           @import "../src/theme/variables.scss";
         `,
       },
       sass: { // 9.0.0
         additionalData: `
+          @use "@daimler/productkit-core/exports/web/styles/scss/variables" as tokens
+          @use "@daimler/productkit-core/exports/web/styles/scss/variables-dark" as tokensDark
           @import "../src/theme/variables.scss"
         `,
       },
