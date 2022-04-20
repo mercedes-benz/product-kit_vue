6f65e8619a235e8af950ff477ddd3540f1936e9f
lukashar <lukas.harsch@mercedes-benz.com>
2022-04-20 15:12:47 +0200

    Updated Product Kit React to 1.3.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 All notable changes to this project will be documented in this file.
 
+# 1.0.1
+
+### Improvements
+
+* Changed color imports from sass to js
+
 # 1.0.0
 
 Initial release. See [readme](https://github.com/mercedes-benz/product-kit_vue/blob/main/README.md) for more information
--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,44 +1,46 @@
 // SPDX-License-Identifier: MIT
 
-const variables = require('./colors.scss')
+import * as tokens from "@daimler/productkit-core/exports/web/styles/js/variables";
+import * as tokensDark from "@daimler/productkit-core/exports/web/styles/js/variables-dark";
+
 require('./overrides.scss')
 
 const colorObject = {
     deepblue: {
-        base: variables.db,
-        lighten1: variables.dbL1,
-        lighten2: variables.dbL2,
-        lighten3: variables.dbL3,
-        lighten4: variables.dbL4,
-        lighten5: variables.dbL5,
-        darken1: variables.dbD1,
-        darken2: variables.dbD2,
-        darken3: variables.dbD3,
-        darken4: variables.dbD4
+        base: tokens.colorBrandDeepblue500,
+        lighten1: tokens.colorBrandDeepblue400,
+        lighten2: tokens.colorBrandDeepblue300,
+        lighten3: tokens.colorBrandDeepblue200,
+        lighten4: tokens.colorBrandDeepblue100,
+        lighten5: tokens.colorBrandDeepblue50,
+        darken1: tokens.colorBrandDeepblue600,
+        darken2: tokens.colorBrandDeepblue700,
+        darken3: tokens.colorBrandDeepblue800,
+        darken4: tokens.colorBrandDeepblue900,
     },
     grayblue: {
-        base: variables.gb,
-        lighten1: variables.gbL1,
-        lighten2: variables.gbL2,
-        lighten3: variables.gbL3,
-        lighten4: variables.gbL4,
-        lighten5: variables.gbL5,
-        darken1: variables.gbD1,
-        darken2: variables.gbD2,
-        darken3: variables.gbD3,
-        darken4: variables.gbD4
+        base: tokens.colorBrandGrayblue500,
+        lighten1: tokens.colorBrandGrayblue400,
+        lighten2: tokens.colorBrandGrayblue300,
+        lighten3: tokens.colorBrandGrayblue200,
+        lighten4: tokens.colorBrandGrayblue100,
+        lighten5: tokens.colorBrandGrayblue50,
+        darken1: tokens.colorBrandGrayblue600,
+        darken2: tokens.colorBrandGrayblue700,
+        darken3: tokens.colorBrandGrayblue800,
+        darken4: tokens.colorBrandGrayblue900,
     },
     neutral: {
-        base: variables['neutral-500'],
-        lighten5: variables['neutral-50'],
-        lighten4: variables['neutral-100'],
-        lighten3: variables['neutral-200'],
-        lighten2: variables['neutral-300'],
-        lighten1: variables['neutral-400'],
-        darken1: variables['neutral-600'],
-        darken2: variables['neutral-700'],
-        darken3: variables['neutral-800'],
-        darken4: variables['neutral-900']
+        base: tokens.colorBrandNeutral500,
+        lighten1: tokens.colorBrandNeutral400,
+        lighten2: tokens.colorBrandNeutral300,
+        lighten3: tokens.colorBrandNeutral200,
+        lighten4: tokens.colorBrandNeutral100,
+        lighten5: tokens.colorBrandNeutral50,
+        darken1: tokens.colorBrandNeutral600,
+        darken2: tokens.colorBrandNeutral700,
+        darken3: tokens.colorBrandNeutral800,
+        darken4: tokens.colorBrandNeutral900,
     },
 }
 
@@ -47,52 +49,55 @@ const shades = ["50", "100", "200", "300", "400", "500", "600", "700", "800", "9
 
 colorNames.forEach(color => {
     shades.forEach(shade => {
-        var name = color + "-" + shade
-        var temp = variables[name]
+        var name = "colorBrand" + color.charAt(0).toUpperCase() + color.slice(1) + shade
+        var name2 = color + "-" + shade
+        var temp = tokens[name]
         var variable = {}
-        variable[name] = temp
+        variable[name2] = temp
         Object.assign(colorObject, variable)
     });
 });
+
+console.log(colorObject)
 
 const colors = {
     theme: {
         themes: {
             light: {
-                primary: variables.primaryL,
-                "primary-contrast": variables.primaryContrastL,
-                secondary: variables.secondaryL,
-                "secondary-contrast": variables.secondaryContrastL,
-                tertiary: variables.tertiaryL,
-                "tertiary-contrast": variables.tertiaryContrastL,
-                quaternary: variables.quaternaryL,
-                "quaternary-contrast": variables.quaternaryContrastL,
-                error: variables.errorL,
-                "error-contrast": variables.errorContrastL,
-                background: variables.backgroundL,
-                "background-contrast": variables.backgroundContrastL,
-                surface: variables.surfaceL,
-                "surface-contrast": variables.surfaceContrastL,
-                outline: variables.outlineL,
-                "outline-contrast": variables.outlineContrastL,
+                primary: tokens.colorApplicationPrimary,
+                "primary-contrast": tokens.colorApplicationContrastPrimary,
+                secondary: tokens.colorApplicationSecondary,
+                "secondary-contrast": tokens.colorApplicationContrastSecondary,
+                tertiary: tokens.colorApplicationTertiary,
+                "tertiary-contrast": tokens.colorApplicationContrastTertiary,
+                quaternary: tokens.colorApplicationQuaternary,
+                "quaternary-contrast": tokens.colorApplicationContrastQuaternary,
+                error: tokens.colorApplicationError,
+                "error-contrast": tokens.colorApplicationContrastError,
+                background: tokens.colorApplicationBackground,
+                "background-contrast": tokens.colorApplicationContrastBackground,
+                surface: tokens.colorApplicationSurface,
+                "surface-contrast": tokens.colorApplicationContrastSurface,
+                outline: tokens.colorApplicationOutline,
+                "outline-contrast": tokens.colorApplicationContrastOutline,
             },
             dark: {
-                primary: variables.primaryD,
-                "primary-contrast": variables.primaryContrastD,
-                secondary: variables.secondaryD,
-                "secondary-contrast": variables.secondaryContrastD,
-                tertiary: variables.tertiaryD,
-                "tertiary-contrast": variables.tertiaryContrastD,
-                quaternary: variables.quaternaryD,
-                "quaternary-contrast": variables.quaternaryContrastD,
-                error: variables.errorD,
-                "error-contrast": variables.errorContrastD,
-                background: variables.backgroundD,
-                "background-contrast": variables.backgroundContrastD,
-                surface: variables.surfaceD,
-                "surface-contrast": variables.surfaceContrastD,
-                outline: variables.outlineD,
-                "outline-contrast": variables.outlineContrastD,
+                primary: tokensDark.colorApplicationPrimary,
+                "primary-contrast": tokensDark.colorApplicationContrastPrimary,
+                secondary: tokensDark.colorApplicationSecondary,
+                "secondary-contrast": tokensDark.colorApplicationContrastSecondary,
+                tertiary: tokensDark.colorApplicationTertiary,
+                "tertiary-contrast": tokensDark.colorApplicationContrastTertiary,
+                quaternary: tokensDark.colorApplicationQuaternary,
+                "quaternary-contrast": tokensDark.colorApplicationContrastQuaternary,
+                error: tokensDark.colorApplicationError,
+                "error-contrast": tokensDark.colorApplicationContrastError,
+                background: tokensDark.colorApplicationBackground,
+                "background-contrast": tokensDark.colorApplicationContrastBackground,
+                surface: tokensDark.colorApplicationSurface,
+                "surface-contrast": tokensDark.colorApplicationContrastSurface,
+                outline: tokensDark.colorApplicationOutline,
+                "outline-contrast": tokensDark.colorApplicationContrastOutline,
             }
         },
         options: {
