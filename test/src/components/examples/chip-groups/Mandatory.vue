51d4e168871571776bd773580f541466912e7276
lukashar <lukas.harsch@mercedes-benz.com>
2022-05-18 14:17:52 +0200

    Improvements

--- test/src/components/examples/chip-groups/Mandatory.vue
+++ test/src/components/examples/chip-groups/Mandatory.vue
@@ -1,36 +1,33 @@
 <!-- SPDX-License-Identifier: MIT -->
 
 <template>
-  <v-row justify="space-around">
-    <v-col cols="12" sm="6" md="4">
-      <v-sheet elevation="10" class="py-4 px-1">
-        <v-chip-group
-          mandatory
-          active-class="primary--text"
-        >
-          <v-chip v-for="tag in tags" :key="tag">
-            {{ tag }}
-          </v-chip>
-        </v-chip-group>
-      </v-sheet>
-    </v-col>
-  </v-row>
+    <v-row justify="space-around">
+        <v-col cols="12" sm="6" md="4">
+            <v-sheet elevation="10" class="py-4 px-1">
+                <v-chip-group mandatory active-class="primary--text">
+                    <v-chip v-for="tag in tags" :key="tag">
+                        {{ tag }}
+                    </v-chip>
+                </v-chip-group>
+            </v-sheet>
+        </v-col>
+    </v-row>
 </template>
 
 <script>
-  export default {
+export default {
     data: () => ({
-      tags: [
-        'Work',
-        'Home Improvement',
-        'Vacation',
-        'Food',
-        'Drawers',
-        'Shopping',
-        'Art',
-        'Tech',
-        'Creative Writing',
-      ],
+        tags: [
+            "Work",
+            "Home Improvement",
+            "Vacation",
+            "Food",
+            "Drawers",
+            "Shopping",
+            "Art",
+            "Tech",
+            "Creative Writing",
+        ],
     }),
-  }
+};
 </script>
