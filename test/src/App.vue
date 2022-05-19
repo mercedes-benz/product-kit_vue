91eef34c5bea2d799cb9795d0cbed832540592f9
lukashar <lukas.harsch@mercedes-benz.com>
2022-05-19 10:44:06 +0200

    Added v-container responsiveness

--- test/src/App.vue
+++ test/src/App.vue
@@ -2,77 +2,81 @@
 
 <template>
     <v-app>
-        <mbti-header title="Vuetify Playground">
-            <template v-slot:logo>
-                <v-img
-                    :src="require('@/assets/mbti.svg')"
-                    class="mr-5"
-                    max-height="60"
-                    max-width="60"
-                    contain
-                ></v-img>
-            </template>
-            <template v-slot:actions>
-                <v-autocomplete
-                    v-model="selectedComponent"
-                    :items="components"
-                    dense
-                    outlined
-                    class="mx-2"
-                    style="width: 0px"
-                    label="Search components"
-                    @change="route()"
-                    hide-details
-                ></v-autocomplete>
-                <v-switch
-                    v-model="darkSwitch"
-                    label="Darkmode"
-                    color="primary"
-                    @change="toggleDarkMode"
-                ></v-switch>
-            </template>
-        </mbti-header>
-        <mbti-nav>
-            <mbti-nav-item
-                icon="mdi-home"
-                title="Home"
-                :to="getRoutePath('/')"
-            />
-            <mbti-nav-item
-                icon="mdi-palette"
-                title="Colors"
-                :to="getRoutePath('colors')"
-            />
-            <mbti-nav-item
-                icon="mdi-format-size"
-                title="Font"
-                :to="getRoutePath('font')"
-            />
-            <mbti-nav-item
-                icon="mdi-grid"
-                title="Layout"
-                :to="getRoutePath('layout')"
-            />
-            <mbti-nav-item
-                icon="mdi-resize"
-                title="Size"
-                :to="getRoutePath('size')"
-            />
-            <mbti-nav-item
-                v-for="item in menuItems"
-                :key="item.title"
-                :title="item.title"
-                :to="getRoutePath(item.title)"
-            />
-        </mbti-nav>
-        <v-main>
-            <router-view />
-        </v-main>
-        <mbti-footer>
-            <v-spacer></v-spacer>
-            <v-btn text> Imprint </v-btn>
-            <v-btn text> Privacy Policy </v-btn>
-        </mbti-footer>
+        <v-container style="min-height: 100%; position: relative">
+            <mbti-header title="Vuetify Playground">
+                <template v-slot:logo>
+                    <v-img
+                        :src="require('@/assets/mbti.svg')"
+                        class="mr-5"
+                        max-height="60"
+                        max-width="60"
+                        contain
+                    ></v-img>
+                </template>
+                <template v-slot:actions>
+                    <v-autocomplete
+                        v-model="selectedComponent"
+                        :items="components"
+                        dense
+                        outlined
+                        class="mx-2"
+                        style="width: 0px"
+                        label="Search components"
+                        @change="route()"
+                        hide-details
+                    ></v-autocomplete>
+                    <v-switch
+                        v-model="darkSwitch"
+                        label="Darkmode"
+                        color="primary"
+                        @change="toggleDarkMode"
+                    ></v-switch>
+                </template>
+            </mbti-header>
+            <mbti-nav>
+                <mbti-nav-item
+                    icon="mdi-home"
+                    title="Home"
+                    :to="getRoutePath('/')"
+                />
+                <mbti-nav-item
+                    icon="mdi-palette"
+                    title="Colors"
+                    :to="getRoutePath('colors')"
+                />
+                <mbti-nav-item
+                    icon="mdi-format-size"
+                    title="Font"
+                    :to="getRoutePath('font')"
+                />
+                <mbti-nav-item
+                    icon="mdi-grid"
+                    title="Layout"
+                    :to="getRoutePath('layout')"
+                />
+                <mbti-nav-item
+                    icon="mdi-resize"
+                    title="Size"
+                    :to="getRoutePath('size')"
+                />
+                <mbti-nav-item
+                    v-for="item in menuItems"
+                    :key="item.title"
+                    :title="item.title"
+                    :to="getRoutePath(item.title)"
+                />
+            </mbti-nav>
+
+            <v-main>
+                <router-view />
+            </v-main>
+
+            <mbti-footer>
+                <v-spacer></v-spacer>
+                <v-btn text> Imprint </v-btn>
+                <v-btn text> Privacy Policy </v-btn>
+            </mbti-footer>
+        </v-container>
     </v-app>
 </template>
 
