9c165774b24e8f35e4162a1bbfc78add75b0f8de
Michael Geyer <118434520+michaelgeyer@users.noreply.github.com>
2023-01-10 14:04:28 +0100

    Release v2.1.0 (#30)

--- test/src/components/Font.vue
+++ test/src/components/Font.vue
@@ -18,7 +18,7 @@
             </code> 
         </v-container>
         <v-subheader> Font-Size </v-subheader>
-        <v-container v-for="t in text" :key="t.name">
+        <v-container class="mb-12" v-for="t in text" :key="t.name">
             <!--<v-subheader>
                 {{ t.name }}
             </v-subheader>-->
@@ -79,16 +79,32 @@ export default {
                 title: "Subtitle 1",
             },
             {
+                name: "subtitle-1-strong",
+                title: "Subtitle 1 Strong",
+            },
+            {
                 name: "subtitle-2",
                 title: "Subtitle 2",
+            },
+            {
+                name: "subtitle-2-strong",
+                title: "Subtitle 2 Strong",
             },
             {
                 name: "body-1",
                 title: "Body 1",
             },
             {
+                name: "body-1-strong",
+                title: "Body 1 Strong",
+            },
+            {
                 name: "body-2",
                 title: "Body 2",
+            },
+            {
+                name: "body-2-strong",
+                title: "Body 2 Strong",
             },
             {
                 name: "button",
@@ -97,6 +113,10 @@ export default {
             {
                 name: "caption",
                 title: "Caption",
+            },
+            {
+                name: "caption-strong",
+                title: "Caption Strong",
             },
             {
                 name: "overline",
