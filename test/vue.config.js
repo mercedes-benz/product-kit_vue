fdcbf366f46b44acfc376d688fa063a9b4d66791
Michael Geyer <118434520+michaelgeyer@users.noreply.github.com>
2023-03-13 16:49:37 +0100

    update to productkit-core v2.2.0 (#41)
    * update productkit-core to v2.2.0
    
    * update token import path using @mercedes-benz

--- test/vue.config.js
+++ test/vue.config.js
@@ -15,16 +15,16 @@ module.exports = {
     loaderOptions: {
       scss: { // 9.0.0
         additionalData: `
-          @use "@daimler/productkit-core/dist/web/styles/mbti/scss/variables" as tokens;
-          @use "@daimler/productkit-core/dist/web/styles/mbti/scss/variables-dark" as tokensDark;
+          @use "@mercedes-benz/productkit-core/dist/web/styles/mbti/scss/variables" as tokens;
+          @use "@mercedes-benz/productkit-core/dist/web/styles/mbti/scss/variables-dark" as tokensDark;
           @use "sass:meta";
           @import "../src/theme/variables.scss";
         `,
       },
       sass: { // 9.0.0
         additionalData: `
-          @use "@daimler/productkit-core/dist/web/styles/mbti/scss/variables" as tokens
-          @use "@daimler/productkit-core/dist/web/styles/mbti/scss/variables-dark" as tokensDark
+          @use "@mercedes-benz/productkit-core/dist/web/styles/mbti/scss/variables" as tokens
+          @use "@mercedes-benz/productkit-core/dist/web/styles/mbti/scss/variables-dark" as tokensDark
           @use "sass:meta"
           @import "../src/theme/variables.scss"
         `,
