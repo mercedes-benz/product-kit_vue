29b161814fac4e3a4455bdfa13d4718dd95e916e
Michael Geyer <michael.m.geyer@mercedes-benz.com>
2023-06-06 14:29:06 +0200

    adds playground env to test package

---
+++ playground/src/main.js
@@ -0,0 +1,21 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import { productKitTheme } from '../../src/index';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    themes: productKitTheme
+  }
+});
+
+const app = createApp(App);
+
+app.use(vuetify);
+app.mount('#app');
