df5ddbaf5fb1d4fd70ab8fdabe267cfe7f887346
Michael Geyer <michael.m.geyer@mercedes-benz.com>
2023-06-07 14:23:04 +0200

    refactors create spacing classes

--- playground/src/components/Spacing.vue
+++ playground/src/components/Spacing.vue
@@ -1,22 +1,12 @@
 <template>
-  <h2 class="text-h2 mb-12">Buttons</h2>
+  <h2 class="text-h2 mb-12">Spacings</h2>
   <div>
-    <div
-      v-for="i in 10"
-      :key="i"
-      class="bg-primary mb-xl"
-    >
-      hello
-    </div>
+    <div class="bg-primary ma-xl pa-l">Box</div>
   </div>
 </template>
 
 <style scoped>
-.bg-primary {
-  /* background-color: blue; */
+/* .bg-primary {
   background-color: var(--v-theme-primary);
-}
-
-.ma-xl {
-}
+} */
 </style>
