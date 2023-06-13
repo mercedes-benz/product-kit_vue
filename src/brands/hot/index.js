3d0ad84ee93b9956ddb152b817d40f4c3112d14d
Michael Geyer <michael.m.geyer@mercedes-benz.com>
2023-06-13 15:21:17 +0200

    add color imports for brands

---
+++ src/brands/hot/index.js
@@ -0,0 +1,4 @@
+import * as light from '@mercedes-benz/productkit-core/dist/web/styles/hot/js/variables.js';
+import * as dark from '@mercedes-benz/productkit-core/dist/web/styles/hot/js/variables-dark.js';
+
+export default { light, dark };
---
+++ src/brands/mb/index.js
@@ -0,0 +1,4 @@
+import * as light from '@mercedes-benz/productkit-core/dist/web/styles/mb/js/variables.js';
+import * as dark from '@mercedes-benz/productkit-core/dist/web/styles/mb/js/variables-dark.js';
+
+export default { light, dark };
