f100ffeb9ce841896eb68d29a3e37265491bdd67
Michael Geyer <michael.m.geyer@mercedes-benz.com>
2023-06-15 09:24:47 +0200

    add defaults for each brand

---
+++ src/brands/hot/defaults.js
@@ -0,0 +1,5 @@
+export default {
+  VApp: {
+    class: 'pk-brand--hot',
+  },
+};
---
+++ src/brands/index.js
@@ -0,0 +1,6 @@
+import hotDefaults from './hot/defaults';
+import mbDefaults from './mb/defaults';
+import mbtiDefaults from './mbti/defaults';
+import mbtmDefaults from './mbtm/defaults';
+
+export { hotDefaults, mbDefaults, mbtiDefaults, mbtmDefaults };
