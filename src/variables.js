8169166bdce818d02d1f459a2096b2cd20322bb5
Michael Geyer <michael.m.geyer@mercedes-benz.com>
2023-06-21 16:15:11 +0200

    remove files and minor updates

--- src/variables.js
+++ src/variables.js
@@ -1,4 +1,3 @@
-// @ts-ignore
 import * as tokens from '@mercedes-benz/productkit-core/dist/web/styles/mbti/js/variables.js';
 
 export const variables = {
@@ -8,5 +7,4 @@ export const variables = {
   'pk-heading4-font-size': tokens.fontApplicationSHeadline4FontSize,
   'pk-heading5-font-size': tokens.fontApplicationSHeadline5FontSize,
   'pk-heading6-font-size': tokens.fontApplicationSHeadline6FontSize,
-  'pk-button-font-weight': tokens.fontApplicationSButtonFontWeight,
 };
