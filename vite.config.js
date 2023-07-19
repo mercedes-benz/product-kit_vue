6df5765c9de26520c732e770a622d0f38adbf231
Michael Geyer <michael.m.geyer@mercedes-benz.com>
2023-07-19 11:14:39 +0200

    add vuetify-settings via vite-vueitfy-plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import vuetify from 'vite-plugin-vuetify';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vuetify({
+      styles: { configFile: '../src/styles/settings.scss' },
+    }),
+  ],
   root: './playground',
 });
