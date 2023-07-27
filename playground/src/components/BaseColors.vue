fd6ee785b7d75db92191cdefc127e7a02cd0b984
Michael Geyer <michael.m.geyer@mercedes-benz.com>
2023-07-27 14:20:44 +0200

    update playground

--- playground/src/components/BaseColors.vue
+++ playground/src/components/BaseColors.vue
@@ -24,14 +24,16 @@ colorNames.forEach((color) => {
 <template>
   <h2 class="text-h2 mb-12">Base Colors</h2>
   <div class="mb-xl">
-    <v-row
-      v-for="color in colors"
-      :key="color"
-    >
-      <v-col :class="color">
-        {{ color }}
-      </v-col>
-      <v-col :class="`${color}-contrast`"> {{ color }}-contrast </v-col>
-    </v-row>
+    <v-container>
+      <v-row
+        v-for="color in colors"
+        :key="color"
+      >
+        <v-col :class="color">
+          {{ color }}
+        </v-col>
+        <v-col :class="`${color}-contrast`"> {{ color }}-contrast </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
