d31a64c32fb02ae3c3bc2adff960aee382a201f6
Michael Geyer <michael.m.geyer@mercedes-benz.com>
2023-07-27 13:44:18 +0200

    update playground

--- playground/src/components/Grid.vue
+++ playground/src/components/Grid.vue
@@ -2,6 +2,9 @@
   <h2 class="text-h2 mb-12">Grid</h2>
   <v-container class="bg-grey">
     <v-row>
+      <v-col cols="12">
+        <p class="text-black ma-2">With Gutter</p>
+      </v-col>
       <v-col
         v-for="n in 12"
         :key="n"
@@ -12,7 +15,24 @@
         xl="2"
         xxl="1"
       >
-        <v-sheet class="ma-2 pa-2 bg-primary">1</v-sheet>
+        <v-sheet class="bg-primary text-center">{{ n }}</v-sheet>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col cols="12">
+        <p class="text-black ma-2">Without Gutter</p>
+      </v-col>
+      <v-col
+        v-for="n in 12"
+        :key="n"
+        cols="12"
+        sm="6"
+        md="4"
+        lg="3"
+        xl="2"
+        xxl="1"
+      >
+        <v-sheet class="bg-primary text-center">{{ n }}</v-sheet>
       </v-col>
     </v-row>
   </v-container>
