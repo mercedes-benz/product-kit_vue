061f924b712bccd009d9b75396aae8622bd6c065
Michael Geyer <michael.m.geyer@mercedes-benz.com>
2023-08-01 13:20:06 +0200

    update playground

--- playground/src/main.js
+++ playground/src/main.js
@@ -5,16 +5,9 @@ import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 
 import { useProductKit } from '../../src/index';
+
 const productKit = useProductKit('mb');
 const vuetify = createVuetify(productKit);
-
-// const vuetify = createVuetify({
-//   theme: {
-//     themes: productKit.themes,
-//   },
-//   defaults: productKit.defaults,
-//   display: productKit.display,
-// });
 
 const app = createApp(App);
 
